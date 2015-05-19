36fc05b22e09f4a80417f01593905b06403b563f
ThomasLB <thmslbrs@gmail.com>
2015-05-19 14:33:37 +0200

    XML
    ajout doc et déplacements  de fichiers

--- XML/XMLfct.py
+++ XML/XMLfct.py
@@ -4,6 +4,7 @@ Created on Mon May 18 11:23:05 2015
 
 @author: Projet
 """
+
 import os
 import ntpath
 import numpy as np
@@ -16,8 +17,17 @@ from xml.dom import minidom
 
 #fonction permettant d'avoir en sortie un arbre XML proprement mis en page
 def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
+    
     """
+    Return a pretty-printed XML string for the Element.
+    
+    :param elem: matrice contenant les vecteurs keypoints obtenus par le descripteur
+    :type elem: string
+    
+    :return: l'arbre XML mis en page
+    :rtype: string
+    """
+    
     rough_string = ElementTree.tostring(elem, 'utf-8')
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
@@ -25,13 +35,26 @@ def prettify(elem):
 #creation arbre XML pour le descripteur en input
 def XMltree(des,name_desc):
     
+    """
+    Creation de l'arbre du XML à partir des
+
+    :param des: matrice contenant les vecteurs keypoints obtenus par le descripteur
+    :type des: array
+    :param name_desc: nom du descripteur utilisé
+    :type name_desc: string
+    
+    :return: l'arbre XML 
+    :rtype: string
+    """    
+    
+    
     #nombre de key-points
     nbKP=np.size(des,0)
     
     #Balise racine avec nom du descripteur
     top = Element(str(name_desc))
     
-    #Balises keypoints
+    # Création des balises keypoints selon le nmbre 
     keypoints = [
         Element('keypoint', num=str(i))
         for i in xrange(nbKP-1)
@@ -45,8 +68,10 @@ def XMltree(des,name_desc):
         kp.text=des[k,:]
         k+=1
     
+    #envoi de l'arbre vers la fonction prettify pour le rendre plus lisible
     tree=prettify(top)
     
+    #print test
     print tree
     
     return tree
@@ -54,9 +79,25 @@ def XMltree(des,name_desc):
 #creation du fichier xml avec arbre xml, chemin image et chemin dossier en input   
 def XMLfile(tree,imgpath,path_desc):
     
+    """
+    Creation du fichier XML dans le dossier du descripteur
+
+    :param tree: matrice contenant les vecteurs keypoints obtenus par le descripteur
+    :type tree: string
+    :param imgpath: chemin du fichier image 
+    :type denominateur: string
+    :param : chemin du dossier du descripteur utilisé
+    :type denominateur: string
+   
+    """
+    
+    #stockage du répertoire de travail actuel
     path=os.getcwd()
     
+    #changement du répertoire de travail pour celui du descripteur utilisé
     os.chdir(path_desc)
+    
+    #récupération du nom du fichier image décrit
     filename=ntpath.basename(imgpath)
     
     # creation fichier xml
@@ -66,5 +107,6 @@ def XMLfile(tree,imgpath,path_desc):
     # Close opened file
     fo.close()
     
+    #retour à l'espace de travail précédent
     os.chdir(path)
     
