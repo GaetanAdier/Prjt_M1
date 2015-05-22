6c2f8ddf783a5be39407d689313b7f1b872f6a66
EtienneCaillaud <etienne.caillaud@etu.univ-poitiers.fr>
2015-05-22 10:37:16 +0200

    C20
    Modifications du code latex et des modalités d'affichages.
    Création d'un fichier latex pour la documentation

--- C2O/C2O.py
+++ C2O/C2O.py
@@ -7,7 +7,13 @@ Created on Mon May 18 17:08:26 2015
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D 
+
+
+
+#######################################################################################################################
+#####################################################Fonctions#########################################################
+#######################################################################################################################
 
 def diff(img,dX,dY):
     """
@@ -23,7 +29,6 @@ def diff(img,dX,dY):
     M = np.float32([[1,0,-dX],[0,1,dY]])
     dst = cv2.warpAffine(img,M,(cols,rows))
     
-    print rows , cols
     
 
     
@@ -34,21 +39,13 @@ def diff(img,dX,dY):
     reMatimg=np.zeros([rows-dY,cols-dX,plans])
     reMatimg=img[dY:rows,0:cols-dX,:]
     
-    rows,cols, plans = reMatdst.shape
-    
-    print rows , cols    
-    
-    cv2.imshow('image decalée',dst)
-    cv2.waitKey(0) 
-    
-    cv2.imshow('image decalée redimensionnee',reMatdst)
-    cv2.waitKey(0)    
-    
-    cv2.imshow('image d origine redimensionnée',reMatimg)
-    cv2.waitKey(0)
+    Res = np.zeros([rows-dY,cols-dX,plans])
+    Res[:,:,0]=reMatimg[:,:,0]-reMatdst[:,:,0]
+    Res[:,:,1]=reMatimg[:,:,1]-reMatdst[:,:,1]
+    Res[:,:,2]=reMatimg[:,:,2]-reMatdst[:,:,2]
  
 
-    return reMatimg-reMatdst
+    return Res
     
     
     
@@ -57,7 +54,12 @@ def diff(img,dX,dY):
 
 def RGBtoLAB(imag):
     
-#    imag = cv2.imread(img,1)
+    """
+    Fonction de BGR à l'espace L*a*b*. Cette fonction utilise le passage par l'espace XYZ. La matrice de passage est speifiee en interne
+    
+    TODO : La fonction n'est pas utilisee pour le moment, le matrice de passage devra être mise en parametre de la fonction ainsi que le blanc de reference utilise
+    """
+    
     rows,cols, plans = imag.shape
 
  
@@ -68,13 +70,35 @@ def RGBtoLAB(imag):
     YBIN=np.zeros((rows,cols))
     ZBIN=np.zeros((rows,cols))
     Lab=np.zeros((rows,cols,3))
+    MatPass = np.zeros((3,3))
+    
+#    0.4887180  0.3106803  0.2006017
+#    0.1762044  0.8129847  0.0108109
+#    0.0000000  0.0102048  0.9897952
+    #Matrice de passage RGB->XYZ
+    MatPass[0,0]=  0.4887180
+    MatPass[0,1]= 0.3106803
+    MatPass[0,2]= 0.2006017
+    MatPass[1,0]= 0.1762044
+    MatPass[1,1]=0.8129847
+    MatPass[1,2]= 0.0108109
+    MatPass[2,0]= 0
+    MatPass[2,1]= 0.0102048
+    MatPass[2,2]=0.9897952
+
+    # Blanc de reference
+    stdIllum = np.zeros(3)  
+    stdIllum[0]= 255.0  
+    stdIllum[1]= 255.0
+    stdIllum[2]= 255.0
+    
+    XYZ[:,:,0] = (imag[:,:,2]*MatPass[0,2] + imag[:,:,1]*MatPass[0,1] + imag[:,:,0]*MatPass[0,0])/stdIllum[0]
+    XYZ[:,:,1] = (imag[:,:,2]*MatPass[1,2] + imag[:,:,1]*MatPass[1,1] + imag[:,:,0]*MatPass[1,0])/stdIllum[1]
+    XYZ[:,:,2] = (imag[:,:,2]*MatPass[2,2] + imag[:,:,1]*MatPass[2,1] + imag[:,:,0]*MatPass[2,0])/stdIllum[2]
+    
 
     
-    XYZ[:,:,0] = imag[:,:,2]*0.618 + imag[:,:,1]*0.177 + imag[:,:,0]*0.205
-    XYZ[:,:,1] = imag[:,:,2]*0.299 + imag[:,:,1]*0.587 + imag[:,:,0]*0.114
-    XYZ[:,:,2] = imag[:,:,2]*0 + imag[:,:,1]*0.056 + imag[:,:,0]*0.944
-    
-    XYZ= XYZ/255.0
+#    XYZ= XYZ/255.0
     
     
     
@@ -85,10 +109,10 @@ def RGBtoLAB(imag):
     test = XBIN-1    
     
 
-    print "min XBIN"
-    print np.min(XBIN)
-    print "min XBIN-1"
-    print np.min(XBIN-1)    
+#    print "min XBIN"
+#    print np.min(XBIN)
+#    print "min XBIN-1"
+#    print np.min(XBIN-1)    
     
     #L
     Lab[:,:,0] = (116*( (XYZ[:,:,1])*YBIN)**(1.0/3.0))-16 -903.3*( (XYZ[:,:,1])* (YBIN-1))
@@ -96,7 +120,7 @@ def RGBtoLAB(imag):
     Lab[:,:,1] = 500*((XBIN*(XYZ[:,:,0])**(1.0/3.0))-(YBIN*(XYZ[:,:,1])**(1.0/3.0))+((YBIN-1)*(XYZ[:,:,1])*(7.787)+(1.0/16.0))-((XBIN-1)*(XYZ[:,:,0])*(7.787)+(1.0/16.0)))
     #B
     Lab[:,:,2] = 300*((YBIN*(XYZ[:,:,1])**(1.0/3.0))-(ZBIN*(XYZ[:,:,2])**(1.0/3.0))+((ZBIN-1)*(XYZ[:,:,2])*(7.787)+(1.0/16.0))-((YBIN-1)*(XYZ[:,:,1])*(7.787)+(1.0/16.0)))
-    print np.max(Lab[:,:,1]) , np.max(Lab[:,:,2])
+#    print np.max(Lab[:,:,1]) , np.max(Lab[:,:,2])
 
     
 
@@ -105,6 +129,10 @@ def RGBtoLAB(imag):
     
 
 def CarthesianToSpheric(Lab):
+    """
+    Fonction de calcul des coordonnees spheriques a partir des coordonnes carthesiennes
+    
+    """
     
     rows,cols, plans = Lab.shape
     Spheric=np.zeros((rows,cols,plans))
@@ -122,6 +150,10 @@ def CarthesianToSpheric(Lab):
 
 
 def SphericToCartesian(Spheric):
+    """
+    Fonction de calcul des coordonnees carthesiennes a partir des coordonnes spheriques
+    
+    """
     rows,cols, plans = Spheric.shape
     Cartesian=np.zeros((rows,cols,plans))
     
@@ -130,121 +162,126 @@ def SphericToCartesian(Spheric):
     Cartesian[:,:,2] = Spheric[:,:,0] * np.cos(Spheric[:,:,2])
     
     
-        
-    
     return Cartesian
  
 
 
 
 
+#######################################################################################################################
+#######################################################Main############################################################
+#######################################################################################################################
 
+#################################
+## Definition de l'image de test#
+#################################
+V =100
+#BGR
+#Test Bleu Jaune
+#mat2 = [[0.0,V,V]]
+#mat1= [[V,0.0,0.0]]
 
-#test = np.zeros((1,1,3))
-#test[0,0,0] = 1
-#test[0,0,1] = 1
-#test[0,0,2] = 1
-#print "Coordonnees d origine"
-#print test
-#
-#teste=CarthesianToSpheric(test)
-#print "Coordonnees spheriques"
-#print teste
-#
-#test2 = SphericToCartesian(teste)
-#
-#print "Coordonnees reconverties"
-#print test2
-
-V = 127.0
+#Test Rouge Vert
+mat1 = [[0.0,V,0.0]]
+mat2= [[0.0,0.0,V]]
 
 imgtest = np.zeros((100,300,3))
-imgtest[0:100,0:30]= [[0.0,V,0.0]]
-imgtest[0:100,30:60]= [[0.0,0.0,V]]
-imgtest[0:100,60:90]= [[0.0,V,0.0]]
-imgtest[0:100,90:120]= [[0.0,0.0,V]]
-imgtest[0:100,120:150]= [[0.0,V,0.0]]
-imgtest[0:100,150:180]= [[0.0,0.0,V]]
-imgtest[0:100,180:210]= [[0.0,V,0.0]]
-imgtest[0:100,210:240]= [[0.0,0.0,V]]
-imgtest[0:100,240:270]= [[0.0,V,0.0]]
-imgtest[0:100,270:300]= [[0.0,0.0,V]]
+imgtest[0:100,0:30]= mat1
+imgtest[0:100,30:60]= mat2
+imgtest[0:100,60:90]= mat1
+imgtest[0:100,90:120]= mat2
+imgtest[0:100,120:150]= mat1
+imgtest[0:100,150:180]= mat2
+imgtest[0:100,180:210]= mat1
+imgtest[0:100,210:240]= mat2
+imgtest[0:100,240:270]= mat1
+imgtest[0:100,270:300]= mat2
 
+
+##################################################################
+###Ouverture de l'image et conrversion en 32bit#######
+##################################################################
 
 #imag = cv2.imread("Food.0006.ppm",1)
 #imag = cv2.imread("test2.jpg",1)
 cv2.imshow('imgtest',imgtest)
 cv2.waitKey(0)
 imgtest=imgtest.astype(np.float32)
+imag=imag.astype(np.float32)
 
-
-#Lab=RGBtoLAB("Food.0006.ppm")
 #Lab=RGBtoLAB(imgtest)
+#Lab=RGBtoLAB(imag)
 
+##################################################################
+#######Passage de l'image dans l'espace Lab#######################
+##################################################################
 
-
-#imag = cv2.imread("Food.0006.ppm",1)
-#imag = cv2.imread("test2.jpg",1)
-#rows,cols, plans = imag.shape
-#
-#Lab=np.zeros((rows,cols,3))
-#
-#cv2.imshow('image d origine',imag)
-#cv2.waitKey(0)
-#imag=imag.astype(np.float32)
-##imag*=(1.0/255.0)
 Lab=cv2.cvtColor(imgtest, cv2.COLOR_BGR2LAB) 
-##
+#Lab=cv2.cvtColor(imag, cv2.COLOR_BGR2LAB) 
+
+#print "L"
+#print Lab[:,:,0]
+###
 #print "Apres la conversion LAB"
 #print np.max(Lab[:,:,0]) , np.max(Lab[:,:,1]) , np.max(Lab[:,:,2])
 
 
-#cv2.imshow('L',Lab[:,:,0]/np.max(Lab[:,:,0]))
-#cv2.waitKey(0)
-#cv2.imshow('a',Lab[:,:,1]/np.max(Lab[:,:,1]))
-#cv2.waitKey(0)
-#cv2.imshow('b',Lab[:,:,2]/np.max(Lab[:,:,2]))
-#cv2.waitKey(0)
 
 
-cv2.imshow('L',Lab[:,:,0])
-cv2.waitKey(0)
-cv2.imshow('a',Lab[:,:,1])
-cv2.waitKey(0)
-cv2.imshow('b',Lab[:,:,2])
-cv2.waitKey(0)
+
+#cv2.imshow('L',Lab[:,:,0])
+#cv2.waitKey(0)
+#cv2.imshow('a',Lab[:,:,1])
+#cv2.waitKey(0)
+#cv2.imshow('b',Lab[:,:,2])
+#cv2.waitKey(0)
+
+##################################################################
+##### Fonction de calcul de la difference#########################
+##################################################################
 LabDiff=0
 LabDiff = diff(Lab,1,0)
 
-print "apres la diff"
-print np.max(Lab[:,:,0]) , np.max(Lab[:,:,1]) , np.max(Lab[:,:,2]) , LabDiff.shape
+#print "apres la diff"
+#print np.max(LabDiff[:,:,0]) , np.max(LabDiff[:,:,1]) , np.max(LabDiff[:,:,2]) , LabDiff.shape
+#
+#cv2.imshow('Image de la difference (luminance)',LabDiff[:,:,0])
+#cv2.waitKey(0)
 
-cv2.imshow('Image de la difference',LabDiff[:,:,0])
-cv2.waitKey(0)
+#SphereCoord=CarthesianToSpheric(LabDiff)
+#
+#
+#print "Max des coordonnes spheriques"
+#print np.max(SphereCoord[:,:,0]) , np.max(SphereCoord[:,:,1]) , np.max(SphereCoord[:,:,2])
 
-SphereCoord=CarthesianToSpheric(LabDiff)
-
-
-print "Max des coordonnes spheriques"
-print np.max(SphereCoord[:,:,0]) , np.max(SphereCoord[:,:,1]) , np.max(SphereCoord[:,:,2])
-
-fig = plt.figure()
-
-ax = fig.add_subplot(111, projection='3d')
 
 L=Lab[:,:,0]
 a=Lab[:,:,1]
 b=Lab[:,:,2]
 
 
-ax.contourf(LabDiff[:,:,1], LabDiff[:,:,2], LabDiff[:,:,0], zdir='z',offset=-190)
-ax.contourf(LabDiff[:,:,1], LabDiff[:,:,2], LabDiff[:,:,0], zdir='y',offset=-190)
-ax.contourf(LabDiff[:,:,1], LabDiff[:,:,2], LabDiff[:,:,0], zdir='x',offset=-190)
+
+#######################################################################################################################
+#####################################################Affichage#########################################################
+#######################################################################################################################
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+
+
+
+
+extends = ["neither", "both", "min", "max"]
 ax.scatter3D(LabDiff[:,:,1], LabDiff[:,:,2], LabDiff[:,:,0])
-
-
-
-cv2.destroyAllWindows()
+ax.set_xlim([-200, 200])
+ax.set_ylim([-200, 200])
+ax.set_zlim([-80, 80])
+ax.contourf(LabDiff[:,:,1], LabDiff[:,:,2], LabDiff[:,:,0], zdir='z',offset=-80)
+ax.contourf(LabDiff[:,:,1], LabDiff[:,:,2], LabDiff[:,:,0], zdir='y',offset=-200)
+ax.contourf(LabDiff[:,:,1], LabDiff[:,:,2], LabDiff[:,:,0], zdir='x',offset=-200)
+#cv2.destroyAllWindows()
 ax.set_xlabel('a')
 ax.set_ylabel('b')
 ax.set_zlabel('L')
+
+plt.show()
