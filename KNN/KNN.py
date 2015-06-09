9f554a711580c2b24000d705a28a8eb76931f5f2
EtienneCaillaud <etienne.caillaud@etu.univ-poitiers.fr>
2015-06-09 16:55:37 +0200

    KNN
    Fonction de calcul de la méthode des k plus proches voisins pour la
    classification
    TODO : Documentation

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -4,33 +4,53 @@ Created on Mon Jun 08 16:48:24 2015
 
 @author: etienne
 """
+import operator
 
 import numpy as np
 from collections import Counter
 
 def KNN(matSig, classId ,k,Sig, dType):
     
-        
+    matSig = np.array(matSig)
+    Sig = np.array(Sig)
     
     matDiff = np.zeros(np.shape(matSig))    
     
-    matDiff = np.sum(np.sqrt((matSig - Sig)**2))
     
-         
-    matDiff = np.sum((matSig-Sig)**2/(matSig+Sig+0.0000001)**2)
+    if dType == 0:     
+        matDiff = np.sqrt((matSig - Sig)**2)
+        matDiff = np.sum(matDiff,axis=1)
+    else:
+        matDiff = np.sum((matSig-Sig)**2/(matSig+Sig+0.0000001)**2,axis=1)
+
         
     
     index = np.argsort(matDiff, axis=-1, kind='quicksort', order=None)
     
 
-    matDiff = matDiff[index]
-    classId = classId[index]
+    classIdn = list(np.zeros(k))
     
-    classId = classId[0:k]
     
-    Occurence = Counter(classId)
+    for i in np.arange(0,k):
+        classIdn[i] = classId[index[i]]
+        
     
-    ClassToAffect = Occurence.most_common(1)
     
-    #Contient le string du nom de la classe a affecter a l'image
-    ClassToAffect = ClassToAffect[0][0]
+    Occurence = Counter(classIdn)
+    
+    Classe = Occurence.most_common(1)
+
+
+    ClassToAffect = Classe[0][0]
+    
+    return ClassToAffect
+        
+test = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],[3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9]]
+sig = [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9]
+
+classIds = [173, 173,173,6969,6969,6969,6969]
+
+
+Test2 = KNN(test, classIds ,3,sig, 1)
+
+print Test2
