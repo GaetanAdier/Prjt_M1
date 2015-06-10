7d75bfccbdd5f9c99229e8fd166416fee06ebf41
ThomasLB <thmslbrs@gmail.com>
2015-06-10 11:39:27 +0200

    getClassID
    recup la classID à partir du XML

---
+++ code/getClassID.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 11:31:05 2015
+
+@author: Projet
+"""
+import xml.etree.ElementTree as ET
+
+def GetClassID(filename):
+   
+    """
+    
+    This function retrieves the ClassID from the metadata file.
+    
+    :param filename: nom du fichier XML
+    :type filename:string
+    
+    :return: the category from the XML file
+    :rtype: string
+     
+    :Example:
+        
+        
+    >>> 
+    >>> 
+     
+    """
+    tree = ET.parse(filename)
+    root = tree.getroot()    
+    
+    return root[4]     
+
+    
+
+    
